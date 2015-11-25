7a8309945bc2f740bef4b1e0daeb4ae3d870f2d0
Daniel Roy Greenfeld <pydanny@gmail.com>
2015-11-24 17:41:19 -0800

    Updating to a production/stable designation

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     zip_safe=False,
     keywords='cached-property',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
@@ -44,5 +44,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
